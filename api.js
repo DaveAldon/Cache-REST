a82c5ead791a8672274bca65ac6756c2b409f8ec
DaveAldon <crawford@mail.gvsu.edu>
2018-02-07 09:18:55 -0500

    Changed api events.

--- api.js
+++ api.js
@@ -6,40 +6,65 @@ var results_table =
   "<tr><td id='about'></td>" +
   "</tr></table";
 
+var token, response = "";
+// Workaround until I can figure out unauthorized web apps
+var pcd = {"Authorization": "Basic " + btoa("pcdrest:pcdrest")}
 // This function uses our token to send an api to spotify in order to get
 // information about an artist
-$.searchArtists = function (query) {
-  $.ajax({
-
-    type: 'GET',
-    crossDomain: true,
-    headers: {
-      'Content-Type': 'application/json',
-    "Access-Control-Allow-Origin": '*',
-    "Authorization": "Basic " + btoa("Admin" + ":" + "tristensteele")
-    },
-    url:"https://localhost:57772/csp/pcd/info",
-      success: function (response) {
-        console.log(response);
-      }
-  });
+$.callRest = function (query, reqToken = false) {
+	var capture = "";
+	$.ajax({
+		headers: { pcd },
+		url:"http://localhost:57772/csp/restserver/" + query,
+    	success: function (response) {
+    		console.log(response)
+    		if(reqToken) {
+    			$.captureToken(response)
+    			$('#token').text(JSON.parse(response).token);
+    		}
+    		else $.capture(response)
+    	}
+	});
 };
+
+// Callbacks for ajax token
+$.captureToken = function (r) {
+	token = JSON.parse(r).token;
+}
+$.capture = function (r) {
+	response = r["token"];
+}
 
 // Event handling
 $(document).ready(function() {
-
-  // Search event where the text box contents are sent to our query
-  // The results are faded into view
+	$("#loginbtn").click(function() {
+		var temp = document.getElementsByTagName("template")[0];
+		  var clon = temp.content.cloneNode(true);
+		  document.body.appendChild(clon);
+	  });
   $("#search").click(function() {
-    $('#results').css('visibility','hidden').hide().fadeIn("slow");
-    if(!jQuery.trim($("#query").val()).length > 0) {
-      $("#query").val('');
-      $('#results').html('<b>Please enter an org\'s name</b>');
-      $('#results').css('visibility','visible').hide().fadeIn("slow");
-      return false;
-    }
-    $.searchArtists($("#query").val());
-    $("#query").val('');
-    $('#results').css('visibility','visible').hide().fadeIn("slow");
+	  //var query = "GetPerson/" + $("#query").val() + "/" + token;
+	  var payload = {
+		  "auth":token,
+		  //"auth":$('#customToken').val(),
+		  "payload": [{
+			  "name": $("#query").val()
+		  }]
+	  };
+	  var query = "validate/" + JSON.stringify(payload);
+	  $.callRest(query);
+  });
+  $("#register").click(function() {
+	  var query = "register/" + $("#regUsername").val() + "/" + $("#regPassword").val() + "/" + $("#sq").val() + "/" + $("#sa").val();
+	  $.callRest(query);
+  });
+  $("#login").click(function() {
+	  var payload = {
+		  "username":$("#loginUsername").val(),
+		  "password":$("#loginPassword").val() 
+	  };
+	  var query = "login/" + JSON.stringify(payload);
+	  $.callRest(query, true);
   });
 });
+
